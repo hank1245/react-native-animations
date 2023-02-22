1adb0f5ce61008f3c2ee159d68f84f14006803ec
Hank Kim <hank29206880@gmail.com>
2023-02-22 19:41:06 +0900

    gestures

--- App.js
+++ App.js
@@ -21,7 +21,9 @@ import Animated, {
   withTiming,
   withSpring,
   withRepeat,
+  useAnimatedGestureHandler,
 } from "react-native-reanimated";
+import { translate } from "@shopify/react-native-skia";
 
 const handleRotation = (progress) => {
   "worklet";
@@ -29,142 +31,42 @@ const handleRotation = (progress) => {
 };
 
 export default function App() {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [panEnabled, setPanEnabled] = useState(false);
-  // const scale = new Animated.Value(1);
-  const translateX = new Animated.Value(0);
-  const translateY = new Animated.Value(0);
-  const width = Dimensions.get("window").width;
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
 
-  const progress = useSharedValue(1);
-  const scale = useSharedValue(2);
+  const panGesture = useAnimatedGestureHandler({
+    onStart: (e, context) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (e, context) => {
+      translateX.value = e.translationX + context.translateX;
+      translateY.value = e.translationY + context.translateY;
+    },
+    onEnd: (e, context) => {
+      if (translateX.value ** 2 + translateY.value ** 2 < 40000) {
+        translateX.value = withSpring(0);
+        translateY.value = withSpring(1);
+      }
+    },
+  });
 
   const reanimatedStyle = useAnimatedStyle(() => {
     return {
-      opacity: progress.value,
-      borderRadius: (progress.value * 100) / 2,
-      transform: [{ scale: scale.value }, { rotate: handleRotation(progress) }],
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
     };
   }, []);
 
-  useEffect(() => {
-    progress.value = withRepeat(withSpring(0.5), 10, true);
-    scale.value = withRepeat(withSpring(1), 10, true);
-  }, []);
-
-  const MOCK_DATA = [
-    {
-      type: "컨트롤러",
-      x: 200,
-      y: 300,
-      macAddress: "D2:6A:D6:EC:94:D9",
-    },
-    {
-      type: "컨트롤러",
-      x: 600,
-      y: 130,
-      macAddress: "CF:0F:27:E2:33:32",
-    },
-    {
-      type: "컨트롤러",
-      x: 700,
-      y: 350,
-      macAddress: "F1:93:11:20:A9:B6",
-    },
-  ];
-
-  const onPinchEvent = Animated.event(
-    [
-      {
-        nativeEvent: { scale },
-      },
-    ],
-    { useNativeDriver: true }
-  );
-  const onPanEvent = Animated.event(
-    [
-      {
-        nativeEvent: {
-          translationX: translateX,
-          translationY: translateY,
-        },
-      },
-    ],
-    { useNativeDriver: true }
-  );
-
-  const handlePinchStateChange = ({ nativeEvent }) => {
-    // enabled pan only after pinch-zoom
-    if (nativeEvent.state === State.ACTIVE) {
-      setPanEnabled(true);
-    }
-
-    // when scale < 1, reset scale back to original (1)
-    const nScale = nativeEvent.scale;
-    if (nativeEvent.state === State.END) {
-      if (nScale < 1) {
-        Animated.spring(scale, {
-          toValue: 1,
-          useNativeDriver: true,
-        }).start();
-        Animated.spring(translateX, {
-          toValue: 0,
-          useNativeDriver: true,
-        }).start();
-        Animated.spring(translateY, {
-          toValue: 0,
-          useNativeDriver: true,
-        }).start();
-        setPanEnabled(false);
-      }
-    }
-  };
-
   return (
     <View style={styles.container}>
-      <Animated.View
-        style={[
-          { height: 100, width: 100, backgroundColor: "blue" },
-          reanimatedStyle,
-        ]}
-      />
+      <View style={styles.circle}></View>
+      <PanGestureHandler onGestureEvent={panGesture}>
+        <Animated.View style={[styles.square, reanimatedStyle]} />
+      </PanGestureHandler>
     </View>
-    // <View style={styles.container}>
-    //   <Image
-    //     source={BackgroundImg}
-    //     style={{ width: width }}
-    //     resizeMode="contain"
-    //   />
-    //   {MOCK_DATA.map((item) => (
-    //     <React.Fragment key={item.macAddress}>
-    //       <PressableScale
-    //         activeScale={0.85}
-    //         onPress={() => setModalVisible(!modalVisible)}
-    //         style={[styles.button, { left: item.x }, { bottom: item.y }]}
-    //       >
-    //         <Text style={styles.text}>{item.type}</Text>
-    //       </PressableScale>
-    //       <Modal
-    //         transparent={true}
-    //         visible={modalVisible}
-    //         onRequestClose={() => setModalVisible(!modalVisible)}
-    //       >
-    //         <View style={styles.centeredView}>
-    //           <View style={styles.modalView}>
-    //             <Text style={styles.modalText}>{item.type}</Text>
-    //             <Text style={styles.modalText}>{item.macAddress}</Text>
-    //             <Pressable
-    //               style={[styles.modalButton, styles.buttonClose]}
-    //               onPress={() => setModalVisible(!modalVisible)}
-    //             >
-    //               <Text style={styles.buttonText}>Hide Modal</Text>
-    //             </Pressable>
-    //           </View>
-    //         </View>
-    //       </Modal>
-    //     </React.Fragment>
-    //   ))}
-    // </View>
   );
 }
 
@@ -177,57 +79,17 @@ const styles = StyleSheet.create({
     overflow: "auto",
     position: "relative",
   },
-  image: {
-    height: "auto",
-  },
-  button: {
-    position: "absolute",
-    borderRadius: "50%",
-    backgroundColor: "white",
+  square: {
     width: 100,
     height: 100,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "rgba(0,0,256,0.5)",
   },
-  text: {
-    fontSize: 20,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-  },
-  modalButton: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
+  circle: {
+    width: 400,
+    height: 400,
+    borderRadius: "400",
+    position: "absolute",
+    borderColor: "green",
+    borderWidth: 10,
   },
 });
