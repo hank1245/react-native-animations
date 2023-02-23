1a0f268ad90c0e7efd396eaa170742d6fcd7e93c
Hank Kim <hank29206880@gmail.com>
2023-02-23 12:54:42 +0900

    interpolate pagination

--- App.js
+++ App.js
@@ -22,74 +22,44 @@ import Animated, {
   withSpring,
   withRepeat,
   useAnimatedGestureHandler,
+  useAnimatedScrollHandler,
 } from "react-native-reanimated";
 import { translate } from "@shopify/react-native-skia";
+import Page from "./components/Page";
 
-const handleRotation = (progress) => {
-  "worklet";
-  return `${progress.value * 2 * Math.PI}rad`;
-};
+const Words = ["Nice", "to", "see", "you"];
 
 export default function App() {
   const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
 
-  const panGesture = useAnimatedGestureHandler({
-    onStart: (e, context) => {
-      context.translateX = translateX.value;
-      context.translateY = translateY.value;
-    },
-    onActive: (e, context) => {
-      translateX.value = e.translationX + context.translateX;
-      translateY.value = e.translationY + context.translateY;
-    },
-    onEnd: (e, context) => {
-      if (translateX.value ** 2 + translateY.value ** 2 < 40000) {
-        translateX.value = withSpring(0);
-        translateY.value = withSpring(1);
-      }
-    },
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    translateX.value = event.contentOffset.x;
   });
 
-  const reanimatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: translateX.value },
-        { translateY: translateY.value },
-      ],
-    };
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <View style={styles.circle}></View>
-      <PanGestureHandler onGestureEvent={panGesture}>
-        <Animated.View style={[styles.square, reanimatedStyle]} />
-      </PanGestureHandler>
-    </View>
+    <Animated.ScrollView
+      pagingEnabled
+      onScroll={scrollHandler}
+      scrollEventThrottle={16}
+      horizontal
+      contentContainerStyle={styles.container}
+    >
+      {Words.map((title, index) => {
+        return (
+          <Page
+            key={index.toString()}
+            title={title}
+            index={index}
+            translateX={translateX}
+          />
+        );
+      })}
+    </Animated.ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    overflow: "auto",
-    position: "relative",
-  },
-  square: {
-    width: 100,
-    height: 100,
-    backgroundColor: "rgba(0,0,256,0.5)",
-  },
-  circle: {
-    width: 400,
-    height: 400,
-    borderRadius: "400",
-    position: "absolute",
-    borderColor: "green",
-    borderWidth: 10,
   },
 });
