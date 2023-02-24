94cc52ec10289b8b60e8acf96d664aaec14b2c15
Hank Kim <hank29206880@gmail.com>
2023-02-24 10:28:33 +0900

    initial commit

--- App.js
+++ App.js
@@ -8,78 +8,15 @@ import Animated, {
   withTiming,
 } from "react-native-reanimated";
 
-const Colors = {
-  dark: {
-    background: "#1E1E1E",
-    circle: "#252525",
-    text: "#F8F8F8",
-  },
-  light: {
-    background: "#F8F8F8",
-    circle: "#FFF",
-    text: "#1E1E1E",
-  },
-};
+const BACKGROUND_COLOR = "#444B6F";
+const BACKGROUND_STROKE_COLOR = "#303858";
+const STROKE_COLOR = "#A6E1FA";
 
-const SWITCH_TRACK_COLOR = {
-  true: "rgba(256, 0, 256, 0.2)",
-  false: "rgba(0,0,0,0.1)",
-};
+const { width, height } = Dimensions.et("window");
 
 export default function App() {
-  const [theme, setTheme] = useState("light");
-
-  const progress = useDerivedValue(() => {
-    return theme === "dark" ? withTiming(1) : withTiming(0);
-  }, [theme]);
-
-  const rStyle = useAnimatedStyle(() => {
-    const backgroundColor = interpolateColor(
-      progress.value,
-      [0, 1],
-      [Colors.light.background, Colors.dark.background]
-    );
-    return { backgroundColor };
-  });
-
-  const rCircleStyle = useAnimatedStyle(() => {
-    const backgroundColor = interpolateColor(
-      progress.value,
-      [0, 1],
-      [Colors.light.circle, Colors.dark.circle]
-    );
-
-    return { backgroundColor };
-  });
-
-  const rTextStyle = useAnimatedStyle(() => {
-    const color = interpolateColor(
-      progress.value,
-      [0, 1],
-      [Colors.light.text, Colors.dark.text]
-    );
-
-    return { color };
-  });
-
-  return (
-    <Animated.View style={[styles.container, rStyle]}>
-      <Animated.Text style={[styles.text, rTextStyle]}>Theme</Animated.Text>
-      <Animated.View style={[styles.circle, rCircleStyle]}>
-        <Switch
-          value={theme === "dark"}
-          onValueChange={(toggled) => {
-            setTheme(toggled ? "dark" : "light");
-          }}
-          trackColor={SWITCH_TRACK_COLOR}
-          thumbColor={"violet"}
-        />
-      </Animated.View>
-    </Animated.View>
-  );
+  return <View style={styles.container}></View>;
 }
-
-const SIZE = Dimensions.get("window").width * 0.7;
 
 const styles = StyleSheet.create({
   container: {
@@ -87,27 +24,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  circle: {
-    width: SIZE,
-    height: SIZE,
-    backgroundColor: "#FFF",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: SIZE / 2,
-    shadowOffset: {
-      width: 0,
-      height: 20,
-    },
-    shadowRadius: 10,
-    shadowOpacity: 0.1,
-    elevation: 8,
-  },
-  text: {
-    fontSize: 70,
-    textTransform: "uppercase",
-    fontWeight: "700",
-    letterSpacing: 14,
-    marginBottom: 35,
   },
 });
