8d6257738f3287a47efaaa035b35e1dfd8cf2afc
Hank Kim <hank29206880@gmail.com>
2023-02-27 15:45:40 +0900

    smoother

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,13 +1,5 @@
-import {
-  Pressable,
-  Image,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  TouchableOpacity,
-} from "react-native";
-import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet } from "react-native";
+import React, { useEffect } from "react";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -17,34 +9,25 @@ import Animated, {
 
 function Item({ item }) {
   const rippleOpacity1 = useSharedValue(1);
-  const scale1 = useSharedValue(0.2);
+  const scale1 = useSharedValue(0.1);
   const rippleOpacity2 = useSharedValue(1);
-  const scale2 = useSharedValue(0.2);
+  const scale2 = useSharedValue(0.1);
   const rippleOpacity3 = useSharedValue(1);
-  const scale3 = useSharedValue(0.2);
+  const scale3 = useSharedValue(0.1);
 
   useEffect(() => {
     setTimeout(() => {
-      rippleOpacity1.value = withRepeat(
-        withTiming(0.1, { duration: 1500 }),
-        -1
-      );
-      scale1.value = withRepeat(withTiming(1, { duration: 1500 }), -1);
+      rippleOpacity1.value = withRepeat(withTiming(0, { duration: 2500 }), -1);
+      scale1.value = withRepeat(withTiming(1, { duration: 2500 }), -1);
     }, 200);
     setTimeout(() => {
-      rippleOpacity2.value = withRepeat(
-        withTiming(0.1, { duration: 1500 }),
-        -1
-      );
-      scale2.value = withRepeat(withTiming(1, { duration: 1500 }), -1);
+      rippleOpacity2.value = withRepeat(withTiming(0, { duration: 2500 }), -1);
+      scale2.value = withRepeat(withTiming(1, { duration: 2500 }), -1);
     }, 400);
     setTimeout(() => {
-      rippleOpacity3.value = withRepeat(
-        withTiming(0.1, { duration: 1500 }),
-        -1
-      );
-      scale3.value = withRepeat(withTiming(1, { duration: 1500 }), -1);
-    }, 600);
+      rippleOpacity3.value = withRepeat(withTiming(0, { duration: 2500 }), -1);
+      scale3.value = withRepeat(withTiming(1, { duration: 2500 }), -1);
+    }, 800);
   }, []);
 
   const rStyle1 = useAnimatedStyle(() => {
@@ -79,9 +62,6 @@ function Item({ item }) {
       <Animated.View
         style={[styles.button, { left: item.x }, { bottom: item.y }, rStyle3]}
       ></Animated.View>
-      <Animated.View
-        style={[styles.core, { left: item.x }, { bottom: item.y }]}
-      />
     </>
   );
 }
@@ -96,7 +76,7 @@ const styles = StyleSheet.create({
   button: {
     position: "absolute",
     borderRadius: 50,
-    backgroundColor: "#1e81b0",
+    backgroundColor: "blue",
     width: 100,
     height: 100,
     justifyContent: "center",
