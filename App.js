effd21cb3163cc42216f5032c1193976af1fbacd
Hank Kim <hank29206880@gmail.com>
2023-02-27 15:19:36 +0900

    파형

--- App.js
+++ App.js
@@ -12,66 +12,21 @@ import {
   PinchGestureHandler,
   TapGestureHandler,
 } from "react-native-gesture-handler";
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  useAnimatedGestureHandler,
-  runOnJS,
-  withTiming,
-  withSpring,
-} from "react-native-reanimated";
+import Animated from "react-native-reanimated";
 import { MOCK_DATA } from "./constant";
 import Item from "./components/Item";
 
-const AnimatedImage = Animated.createAnimatedComponent(Image);
 const { width, height } = Dimensions.get("window");
 
 export default function App() {
-  const scale = useSharedValue(1);
-  const focalX = useSharedValue(0);
-  const focalY = useSharedValue(0);
-
-  const pinchHandler = useAnimatedGestureHandler({
-    onStart: () => {},
-    onActive: (event, context) => {
-      scale.value = event.scale;
-      focalX.value = event.focalX;
-      focalY.value = event.focalY;
-    },
-    onEnd: () => {
-      scale.value = withTiming(1);
-    },
-  });
-
-  const reanimatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: focalX.value },
-        { translateY: focalY.value },
-        { translateX: -width / 2 },
-        { translateY: -height / 2 },
-        { scale: scale.value },
-        { translateX: -focalX.value },
-        { translateY: -focalY.value },
-        { translateX: width / 2 },
-        { translateY: height / 2 },
-      ],
-    };
-  });
-
   return (
     <View style={styles.container}>
-      <PinchGestureHandler onGestureEvent={pinchHandler}>
-        <Animated.View style={styles.animationContainer}>
-          <AnimatedImage
-            source={BackgroundImg}
-            style={[styles.background, reanimatedStyle]}
-          />
-          {MOCK_DATA.map((item, index) => (
-            <Item key={index} item={item} />
-          ))}
-        </Animated.View>
-      </PinchGestureHandler>
+      <Animated.View style={styles.animationContainer}>
+        <Image source={BackgroundImg} style={[styles.background]} />
+        {MOCK_DATA.map((item, index) => (
+          <Item key={index} item={item} />
+        ))}
+      </Animated.View>
     </View>
   );
 }
---
+++ components/Item.tsx
@@ -0,0 +1,115 @@
+import {
+  Pressable,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
+
+function Item({ item }) {
+  const rippleOpacity1 = useSharedValue(1);
+  const scale1 = useSharedValue(0.2);
+  const rippleOpacity2 = useSharedValue(1);
+  const scale2 = useSharedValue(0.2);
+  const rippleOpacity3 = useSharedValue(1);
+  const scale3 = useSharedValue(0.2);
+
+  useEffect(() => {
+    setTimeout(() => {
+      rippleOpacity1.value = withRepeat(
+        withTiming(0.1, { duration: 1500 }),
+        -1
+      );
+      scale1.value = withRepeat(withTiming(1, { duration: 1500 }), -1);
+    }, 200);
+    setTimeout(() => {
+      rippleOpacity2.value = withRepeat(
+        withTiming(0.1, { duration: 1500 }),
+        -1
+      );
+      scale2.value = withRepeat(withTiming(1, { duration: 1500 }), -1);
+    }, 400);
+    setTimeout(() => {
+      rippleOpacity3.value = withRepeat(
+        withTiming(0.1, { duration: 1500 }),
+        -1
+      );
+      scale3.value = withRepeat(withTiming(1, { duration: 1500 }), -1);
+    }, 600);
+  }, []);
+
+  const rStyle1 = useAnimatedStyle(() => {
+    return {
+      opacity: rippleOpacity1.value,
+      transform: [{ scale: scale1.value }],
+    };
+  });
+
+  const rStyle2 = useAnimatedStyle(() => {
+    return {
+      opacity: rippleOpacity2.value,
+      transform: [{ scale: scale2.value }],
+    };
+  });
+
+  const rStyle3 = useAnimatedStyle(() => {
+    return {
+      opacity: rippleOpacity3.value,
+      transform: [{ scale: scale3.value }],
+    };
+  });
+
+  return (
+    <>
+      <Animated.View
+        style={[styles.button, { left: item.x }, { bottom: item.y }, rStyle1]}
+      ></Animated.View>
+      <Animated.View
+        style={[styles.button, { left: item.x }, { bottom: item.y }, rStyle2]}
+      ></Animated.View>
+      <Animated.View
+        style={[styles.button, { left: item.x }, { bottom: item.y }, rStyle3]}
+      ></Animated.View>
+      <Animated.View
+        style={[styles.core, { left: item.x }, { bottom: item.y }]}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+  },
+  button: {
+    position: "absolute",
+    borderRadius: 50,
+    backgroundColor: "#1e81b0",
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  core: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: "#063970",
+    position: "absolute",
+    transform: [{ scale: 0.15 }],
+  },
+});
+
+export default Item;
