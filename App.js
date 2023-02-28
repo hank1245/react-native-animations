02c96df683a367ff87ad91d437239ba6ecbbb147
Hank Kim <hank29206880@gmail.com>
2023-02-28 16:38:55 +0900

    setup

--- App.js
+++ App.js
@@ -7,44 +7,19 @@ import {
   Dimensions,
   Modal,
 } from "react-native";
-import BackgroundImg from "./assets/image.jpeg";
-import React, { useRef, useState } from "react";
-import Animated from "react-native-reanimated";
-import { MOCK_DATA } from "./constant";
-import Item from "./components/Item";
 
-const { width, height } = Dimensions.get("window");
+const LIST_ITEM_COLOR = "#1798DE";
+
+const items = new Array(10).fill(0).map((_, index) => ({ id: index }));
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Animated.View style={styles.animationContainer}>
-        <Image source={BackgroundImg} style={[styles.background]} />
-        {MOCK_DATA.map((item, index) => (
-          <React.Fragment key={item.macAddress}>
-            <Item key={index} item={item} />
-          </React.Fragment>
-        ))}
-      </Animated.View>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#4f5555",
     alignItems: "center",
     justifyContent: "center",
-  },
-  animationContainer: {
-    flex: 1,
-    backgroundColor: "#4f5555",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  background: {
-    width: width,
-    height: height,
   },
 });
