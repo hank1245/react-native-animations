05bb3f0a6776ba1c21c64b082ed3c0f5f34af8bf
Hank Kim <hank29206880@gmail.com>
2023-03-02 17:25:34 +0900

    초기화

--- App.js
+++ App.js
@@ -8,10 +8,6 @@ import {
   Modal,
 } from "react-native";
 
-const LIST_ITEM_COLOR = "#1798DE";
-
-const items = new Array(10).fill(0).map((_, index) => ({ id: index }));
-
 export default function App() {
   return <View style={styles.container}></View>;
 }
