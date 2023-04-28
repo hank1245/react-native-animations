cbd51a73cb80bb8b87abab327dfffe1cd2aa6449
Hank Kim <hank29206880@gmail.com>
2023-04-28 13:07:45 +0900

    add typescript

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { FlatList, StyleSheet, View } from "react-native";
+
+export default function App() {}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
