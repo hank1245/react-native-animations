f0a4170386ae8129f06303723787196d66926ecf
Hank Kim <hank29206880@gmail.com>
2023-05-03 14:53:48 +0900

    pangesturehandler

--- App.tsx
+++ App.tsx
@@ -1,62 +1,45 @@
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Dimensions } from "react-native";
+import { translate } from "@shopify/react-native-skia";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, StyleSheet, Dimensions, SafeAreaView } from "react-native";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
 import Animated, {
-  Easing,
-  useAnimatedProps,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
   useSharedValue,
 } from "react-native-reanimated";
-import { withRepeat } from "react-native-reanimated";
-import { useAnimatedStyle } from "react-native-reanimated";
-import { withTiming } from "react-native-reanimated";
-import { Svg, Circle, Path } from "react-native-svg";
 
 const { width, height } = Dimensions.get("window");
 
-const AnimatedCircle = Animated.createAnimatedComponent<any>(Circle);
-
 export default function App() {
-  const progress = useSharedValue(0);
-  const acc = useSharedValue(1);
-
-  const circum = 300;
-  const r = circum / (2 * Math.PI);
-
-  const animatedProps = useAnimatedProps(() => ({
-    strokeDashoffset: circum * progress.value,
-  }));
-
-  useEffect(() => {
-    progress.value = withRepeat(
-      withTiming(3, { duration: 2000, easing: Easing.linear }),
-      -1
-    );
-  }, []);
+  const translateX = useSharedValue(0);
+  const onPanGesture = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>(
+    {
+      onActive: (event) => {
+        translateX.value = event.translationX;
+      },
+    }
+  );
 
   return (
-    <View style={styles.container}>
-      <Svg>
-        <AnimatedCircle
-          cx={width / 2}
-          cy={height / 2}
-          r={r}
-          stroke={"green"}
-          strokeWidth={10}
-          strokeDasharray={50}
-          animatedProps={animatedProps}
-        />
-        <Path
-          d={`M${width / 2 - 150},${height / 2 - 80},${width / 2 + 150},${
-            height / 2 - 80
-          },${width / 2},${height / 2 + 150}z`}
-          fill={"none"}
-          stroke={"black"}
-          strokeWidth={20}
-        />
-      </Svg>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="inverted" />
+      <GestureHandlerRootView>
+        <PanGestureHandler onGestureEvent={onPanGesture}>
+          <Animated.View style={{ backgroundColor: "white", width, height }} />
+        </PanGestureHandler>
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: "#1e1e23",
+  },
 });
