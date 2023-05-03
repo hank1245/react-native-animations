0dbc87f53c8301ea604b6a92ad39d773ca605d55
Hank Kim <hank29206880@gmail.com>
2023-05-03 14:14:31 +0900

    heart animation

--- App.tsx
+++ App.tsx
@@ -1,62 +1,109 @@
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Dimensions } from "react-native";
+import React, { useRef } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ImageBackground,
+  Dimensions,
+  Image,
+} from "react-native";
+import {
+  TapGestureHandler,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 import Animated, {
-  Easing,
-  useAnimatedProps,
+  useAnimatedStyle,
   useSharedValue,
+  withDelay,
+  withSpring,
+  withTiming,
 } from "react-native-reanimated";
-import { withRepeat } from "react-native-reanimated";
-import { useAnimatedStyle } from "react-native-reanimated";
-import { withTiming } from "react-native-reanimated";
-import { Svg, Circle, Path } from "react-native-svg";
 
-const { width, height } = Dimensions.get("window");
-
-const AnimatedCircle = Animated.createAnimatedComponent<any>(Circle);
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+const AnimatedText = Animated.createAnimatedComponent(Text);
 
 export default function App() {
-  const progress = useSharedValue(0);
-  const acc = useSharedValue(1);
+  const doubleTapRef = useRef<any>();
+  const size = useSharedValue(0);
+  const opacity = useSharedValue(0);
+  const onDoubleTap = () => {
+    size.value = withSpring(1, undefined, (isFinished) => {
+      if (isFinished) {
+        size.value = withDelay(500, withTiming(0));
+      }
+    });
+  };
+  const onSingleTap = () => {
+    if (opacity.value === 1) {
+      opacity.value = withTiming(0);
+    }
+    if (opacity.value === 0) {
+      opacity.value = withTiming(1);
+    }
+  };
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: size.value }],
+    };
+  });
 
-  const circum = 300;
-  const r = circum / (2 * Math.PI);
-
-  const animatedProps = useAnimatedProps(() => ({
-    strokeDashoffset: circum * progress.value,
-  }));
-
-  useEffect(() => {
-    progress.value = withRepeat(
-      withTiming(3, { duration: 2000, easing: Easing.linear }),
-      -1
-    );
-  }, []);
+  const textStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
 
   return (
     <View style={styles.container}>
-      <Svg>
-        <AnimatedCircle
-          cx={width / 2}
-          cy={height / 2}
-          r={r}
-          stroke={"green"}
-          strokeWidth={10}
-          strokeDasharray={50}
-          animatedProps={animatedProps}
-        />
-        <Path
-          d={`M${width / 2 - 150},${height / 2 - 80},${width / 2 + 150},${
-            height / 2 - 80
-          },${width / 2},${height / 2 + 150}z`}
-          fill={"none"}
-          stroke={"black"}
-          strokeWidth={20}
-        />
-      </Svg>
+      <GestureHandlerRootView>
+        <TapGestureHandler onActivated={onSingleTap} waitFor={doubleTapRef}>
+          <TapGestureHandler
+            ref={doubleTapRef}
+            maxDelayMs={250}
+            numberOfTaps={2}
+            onActivated={onDoubleTap}
+          >
+            <Animated.View>
+              <ImageBackground
+                style={styles.image}
+                source={require("./assets/image.jpeg")}
+              >
+                <AnimatedImage
+                  source={require("./assets/heart.png")}
+                  style={[styles.image, rStyle]}
+                  resizeMode={"center"}
+                ></AnimatedImage>
+              </ImageBackground>
+              <AnimatedText style={[styles.text, textStyle]}>
+                구독과 좋아요!
+              </AnimatedText>
+            </Animated.View>
+          </TapGestureHandler>
+        </TapGestureHandler>
+      </GestureHandlerRootView>
     </View>
   );
 }
 
+const { width, height } = Dimensions.get("window");
+
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: width,
+    height: width,
+  },
+  text: {
+    fontSize: 60,
+    position: "absolute",
+    top: height / 4,
+    left: width / 3,
+    fontWeight: "700",
+    color: "orange",
+  },
 });
