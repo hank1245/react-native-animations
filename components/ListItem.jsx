ccd79657fcaa58e5645bf457ade06274b07d251b
Hank Kim <hank29206880@gmail.com>
2023-05-04 11:19:49 +0900

    useAnimatedStyle hook

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -3,23 +3,9 @@ import Animated, {
   useAnimatedStyle,
   withTiming,
 } from "react-native-reanimated";
-
+import useAnimatedVisibility from "../hooks/useAnimationVisibility";
 function ListItem({ item, viewableItems }) {
-  const rStyle = useAnimatedStyle(() => {
-    const isVisible = Boolean(
-      viewableItems.value
-        .filter((item) => item.isViewable)
-        .find((viewableItem) => viewableItem.item.id === item.id)
-    );
-    return {
-      opacity: withTiming(isVisible ? 1 : 0),
-      transform: [
-        {
-          scale: withTiming(isVisible ? 1 : 0.6),
-        },
-      ],
-    };
-  });
+  const rStyle = useAnimatedVisibility(viewableItems, item);
 
   return (
     <Animated.View
---
+++ hooks/useAnimationVisibility.js
@@ -0,0 +1,21 @@
+import { useAnimatedStyle, withTiming } from "react-native-reanimated";
+
+export default function useAnimatedVisibility(viewableItems, item) {
+  const rStyle = useAnimatedStyle(() => {
+    const isVisible = Boolean(
+      viewableItems.value
+        .filter((item) => item.isViewable)
+        .find((viewableItem) => viewableItem.item.id === item.id)
+    );
+    return {
+      opacity: withTiming(isVisible ? 1 : 0),
+      transform: [
+        {
+          scale: withTiming(isVisible ? 1 : 0.6),
+        },
+      ],
+    };
+  });
+
+  return rStyle;
+}
